3bf6cd32dc34104715465943e9e470f21bc7d4f2
Muhammad Choerul Chamdani <mkhairulhamdani8@gmail.com>
2024-12-10 22:33:47 +0700

    Update G1D021037_Muhammad Choerul Chamdani_Optimisasi Linear dan Nonlinear.jl

--- G1D021037_Muhammad Choerul Chamdani_Optimisasi Linear dan Nonlinear.jl
+++ G1D021037_Muhammad Choerul Chamdani_Optimisasi Linear dan Nonlinear.jl
@@ -1,33 +1,50 @@
-#Network Flow Problem
 using JuMP
-using GLPK
 using Ipopt
 
-# Matriks P
-P = [13 12 -2; 12 17 6; 2 6 12]
-
-# Vektor q
-q = [-22.0, -14.5, 13.0]
-
-# Konstanta r
-r = 1.0
-
-# Membuat model optimisasi
 model = Model(Ipopt.Optimizer)
 
-# Variabel keputusan
-@variable(model, -1 <= x[1:3] <= 1)
+@variable(model, x1 >= 0)
+@variable(model, x2 >= 0)
 
-# Fungsi tujuan
-@objective(model, Min, 0.5 * sum(P[i, j] * x[i] * x[j] for i in 1:3, j in 1:3) + q' * x + r)
+@constraint(model, 2*x1 + x2 >= 1)
+@constraint(model, x1 + 3*x2 >= 1)
 
-# Menyelesaikan masalah
+#1a
+println("1(a) f0(x1, x2) = x1 + x2")
+@objective(model, Min, (x1 + x2))
 optimize!(model)
+objective_value(model)
+println("Optimal Value for (a): ", objective_value(model))
+println("Optimal Solution for (e): (", value.(x1), ", ", value.(x2), ")")
 
-# Mendapatkan hasil
-optimal_value = objective_value(model)
-optimal_x = value.(x)
+#1b
+println("1(b) f0(x1, x2) = x1 - x2")
+@objective(model, Min, (-x1 -x2))
+optimize!(model)
+objective_value(model)
+println("Optimal Value for (b): ", objective_value(model))
+println("Optimal Solution for (e): (", value.(x1), ", ", value.(x2), ")")
 
-# Menampilkan hasil
-println("Nilai optimal dari fungsi tujuan: ", optimal_value)
-println("Solusi optimal x: ", optimal_x)
+#1c
+println("1(c) f0(x1, x2) = x1")
+@objective(model, Min, (x1))
+optimize!(model)
+objective_value(model)
+println("Optimal Value for (c): ", objective_value(model))
+println("Optimal Solution for (e): (", value.(x1), ", ", value.(x2), ")")
+
+#1d
+println("1(d) f0(x1, x2) = max{x1, x2}")
+@objective(model, Min, x1)
+optimize!(model)
+objective_value(model)
+println("Optimal Value for (d): ", objective_value(model))
+println("Optimal Solution for (e): (", value.(x1), ", ", value.(x2), ")")
+
+#1e
+println("1(e) f0(x1, x2) = (x1)^2 + 9*(x2)^2}")
+@objective(model, Min, (x1)^2 + 9*(x2)^2)
+optimize!(model)
+objective_value(model)
+println("Optimal Value for (e): ", objective_value(model))
+println("Optimal Solution for (e): (", value.(x1), ", ", value.(x2), ")")
